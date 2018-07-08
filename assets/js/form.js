14652daa75902ed6a2e4f9741eb4615a4e892a63
Akihito Morita <moriakijp@gmail.com>
2018-07-09 02:15:15 +0900

    Update assets/js/form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -52,7 +52,7 @@ var margin = {
   right: width * 0
 }
 
-d3.json("data/qwerty4.json", function (errer, data) {
+d3.json("data/qwerty.json", function (errer, data) {
 
   var svg = d3.select("#heatmap").append("svg")
     .attr("width", width)
@@ -70,55 +70,81 @@ d3.json("data/qwerty4.json", function (errer, data) {
 
   let arr = (new Array(4)).fill(1).map((v, i) => v + i)
 
-  console.log(arr);
-  console.log(data);
-  console.log(data.length);
+  // console.log(arr);
+  // console.log(data);
+  // console.log(function(d){return d.ro/w});
+  // var row = data.map(function(d){ return d.row; })
+  // for(let k in data) {
+  //   if(data.hasOwnProperty(k)) {
+  //     console.log(k+ ':' + data[k]);
+  //   }
+  // }
+  for ( let r in data ) {
+   var row = (data[r].row);
 
   heatmap.append('rect')
     .attr("class", "block")
     .attr("x", function (d, i) {
-    return i * blocksize;
+    return i * blocksize / row ;
   })
-  .attr("y", blocksize * 1 )
+  .attr("y", row * blocksize )
   .attr("width", blocksize)
   .attr("height", blocksize)
   .attr("fill", function (d) {
     return colorScale(d.value);
   });
-  heatmap.append('rect')
-  .attr("class", "block")
+
+  heatmap.append('text')
+  .text(function (d) {
+    return d.key
+  })
   .attr("x", function (d, i) {
-  return i * blocksize;
-})
-.attr("y", blocksize * 2)
-.attr("width", blocksize)
-.attr("height", blocksize)
-.attr("fill", function (d) {
-  return colorScale(d.value);
-});
-
-heatmap.append('rect')
-.attr("class", "block")
-.attr("x", function (d, i) {
-return i * blocksize;
-})
-.attr("y", blocksize * 3)
-.attr("width", blocksize)
-.attr("height", blocksize)
-.attr("fill", function (d) {
-return colorScale(d.value);
-});
-
-  heatmap.append('rect')
-    .attr("class", "block")
-    .attr("x", function (d, i) {
-    return i * blocksize;
+    return i * blocksize
   })
-  .attr("y", blocksize * 4 )
-  .attr("width", blocksize)
-  .attr("height", blocksize)
-  .attr("fill", function (d) {
-    return colorScale(d.value);
+  .attr("y", blocksize * 1.5)
+  .attr("text-anchor", "middle")
+  .attr("fill", "#333")
+  .attr("dy", ".35em")
+  .attr("dx", blocksize / 2)
+  .style({
+    "font-size": 25
   });
+
+  }
+
+
+  // var xLabels = svg.selectAll(".Label")
+  //   .data(data)
+  //   .enter().append("text")
+  //   .text(function (d) {
+  //     return d.key;
+  //   })
+  //   .attr("y", 0)
+  //   .attr("x", function (d, i) {
+  //     return i%13 * blocksize ;
+  //   })
+  //   .attr("fill", "#333")
+  //   .style({
+  //     "font-size": 15
+  //   })
+  //   .style("text-anchor", "middle")
+  //   .attr("transform", "translate(60," + blocksize / 1.1 + ")");
+
+  // var yLabels = svg.selectAll(".Label")
+  //   .data(data)
+  //   .enter().append("text")
+  //   .text(function (d) {
+  //     return String(d.row);
+  //   })
+  //   .attr("fill", "#333")
+  //   .attr("x", 0)
+  //   .attr("y", function (d, i) {
+  //     return i  * blocksize;
+  //   })
+  //   .style({
+  //     "font-size": 15
+  //   })
+  //   .style("text-anchor", "end")
+  //   .attr("transform", "translate(-20," + blocksize * 1.6 + ")");
 
 });
