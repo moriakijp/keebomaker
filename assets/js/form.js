9381de272b42b8410fe9295e2aeab3c169276be1
Akihito Morita <moriakijp@gmail.com>
2018-07-09 22:35:38 +0900

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -26,50 +26,55 @@ upload.addEventListener('change', () => {
 
 var sample = document.getElementById('sample-button');
 sample.addEventListener('click', () => {
+  d3.select("svg").remove();
   console.log(uploadedText);
 });
 /* Sample */
 
 // var sample = document.getElementById('sample-button');
 // sample.addEventListener('click', () => {
-  // uploadText = document.getElementById('upload-text');
-  // var ajax = new XMLHttpRequest();
-  // ajax.open("get", "data/sample.txt", false)
-  // response.setContentType("text/plain");
-  // ajax.addEventListener("load", ()=>{console.log(this.response);},false);
-  // ajax.send();
-  // reader = new FileReader('data/sample.txt');
-  // reader.readAsDataURL();
-  // console.log(reader.result);
-  // uploadText.onload = () => () {
-    // uploadText.innerHTML = reader.result;
-    // uploadedText = reader.result;
-    // }
-    // })
+// uploadText = document.getElementById('upload-text');
+// var ajax = new XMLHttpRequest();
+// ajax.open("get", "data/sample.txt", false)
+// response.setContentType("text/plain");
+// ajax.addEventListener("load", ()=>{console.log(this.response);},false);
+// ajax.send();
+// reader = new FileReader('data/sample.txt');
+// reader.readAsDataURL();
+// console.log(reader.result);
+// uploadText.onload = () => () {
+// uploadText.innerHTML = reader.result;
+// uploadedText = reader.result;
+// }
+// })
 
 
-    /* Display heatmap */
+/* Display heatmap */
 
-    var blocksize = 65;
-    var width = 960;
-    var height = 400;
-    var margin = {
-      top: height * 0.05,
-      bottom: height * 0.05,
-      left: width * 0.05,
-      right: width * 0.05
-    }
+var width = self.innerWidth;
+var height = self.innerHeight;
+var margin = {
+  top: height * 0.1,
+  bottom: height * 0.1,
+  left: width * 0.1,
+  right: width * 0.1
+}
+var blocknum = 14;
+var blocksize = (width / blocknum) * 0.8;
 
-    var display = document.getElementById('display-button');
-    display.addEventListener('click', () => {
+var display = document.getElementById('display-button');
+display.addEventListener('click', () => {
 
-      /* character match detection */
-      d3.json("data/qwerty.json", (errer, data) => {
-        uploadedText = document.getElementById('upload-text').value;
-    var character=Array.from(uploadedText);
-    for(var j in character){
-      for(var i in data){
-       if( character[j].toUpperCase() == data[i].char ) {data[i].val++; console.log(data[i].val);}
+  /* character match detection */
+  d3.json("data/qwerty.json", (errer, data) => {
+    uploadedText = document.getElementById('upload-text').value;
+    var character = Array.from(uploadedText);
+    for (var j in character) {
+      for (var i in data) {
+        if (character[j].toUpperCase() == data[i].char) {
+          data[i].val++;
+          /*console.log(data[i].val);*/
+        }
       }
     }
 
@@ -106,39 +111,39 @@ sample.addEventListener('click', () => {
     heatmap.append('rect')
       .attr("class", "block")
       .attr("x", (d, r) => {
-        return (r % 14) * blocksize;
+        return blocksize * ((r % 14) - 1);
       })
       .attr("y", (d, r) => {
-        return blocksize * data[r].row;
+        return blocksize * (data[r].row - 1);
       })
       .attr("width", blocksize)
       .attr("height", blocksize)
-      .attr("rx",200)
-      .attr("ry",200)
-      .attr("fill", (d) =>  {
-        return colorScale(d.val);
+      .attr("rx", 50)
+      .attr("ry", 50)
+      .attr("fill", (d) => {
+        return (d.char) ? colorScale(d.val) : '#FFFFFF';
       })
       .transition()
-      .delay(function(d,i){return i * 100})
-      .duration(1000)
-      .ease("bounce")
-      .attr("rx",10)
-      .attr("ry",10);
+      // .delay(function(d,i){return i * 100})
+      .duration(2000)
+      .ease("elastic")
+      .attr("rx", 0)
+      .attr("ry", 0);
 
     heatmap.append('text')
       .text((d) => {
         return d.char;
       })
       .attr("x", (d, r) => {
-        return (r % 14) * blocksize;
+        return blocksize * ((r % 14) - 1);
       })
       .attr("y", (d, r) => {
-        return blocksize * data[r].row;
+        return blocksize * (data[r].row - 1);
       })
       .attr("text-anchor", "middle")
       .attr("fill", "#333")
-      .attr("dy", blocksize / 2)
       .attr("dx", blocksize / 2)
+      .attr("dy", blocksize / 2)
       .style({
         "font-size": 25
       });
@@ -147,35 +152,35 @@ sample.addEventListener('click', () => {
       .data(data)
       .enter().append("text")
       .text((d) => {
-        return "C"+String(d.col);
+        return "C" + String(d.col);
       })
       .attr("y", 0)
       .attr("x", (d, i) => {
-        return (i % 14-1) * blocksize;
+        return blocksize * ((i % blocknum) - 0.5);
       })
       .attr("fill", "#333")
       .style({
         "font-size": 15
       })
       .style("text-anchor", "middle")
-      .attr("transform", "translate(100," + blocksize / 1.5 + ")");
+      .attr("transform", "translate(margin-left," + blocksize + ")");
 
     var yLabels = svg.selectAll(".Label")
       .data(data)
       .enter().append("text")
-      .text((d) =>{
-        return "R"+String(d.row);
+      .text((d) => {
+        return "R" + String(d.row);
       })
       .attr("fill", "#333")
-      .attr("x", 0)
+      .attr("x", -1 * blocksize)
       .attr("y", (d, i) => {
-        return blocksize * (data[i].row-1);
+        return blocksize * (data[i].row - 0.5);
       })
       .style({
         "font-size": 15
       })
-      .style("text-anchor", "middle")
-      .attr("transform", "translate(-20," + blocksize * 1.5 + ")");
+      .style("text-anchor", "end")
+      .attr("transform", "translate(margin-left," + blocksize + ")");
 
   });
 
